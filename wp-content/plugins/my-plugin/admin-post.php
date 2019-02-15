77dad448265b94ca5936f35ac8a7df2cfa0cac43
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-02-15 11:24:24 -0200

    atualizando plugin

--- wp-content/plugins/my-plugin/admin-post.php
+++ wp-content/plugins/my-plugin/admin-post.php
@@ -1,19 +1,21 @@
 <?php
-    echo"<h3>Mensagem enviada</h3>";
-
+    
     echo"<p>EMAIL: ".$_POST['email']."</p>";
     echo"<p>ASSUNTO: ".$_POST['assunto']."</p>";
     echo"<p>MENSAGEM: ".$_POST['mensagem']."</p>";
 
-    $emaildestinatario = $_POST['email'];
-    $headers           = "From: itjhonatan01@gmail.com\n";
-    $emailsender       = "From: itjhonatan01@gmail.com\n";
+    $emaildestinatario = "itjhonatan01@gmail.com";
+    $headers           = "From: ".$_POST['email']."\n";
+    $emailsender       = "From: ".$_POST['email']."\n";
     $assunto           = $_POST['assunto'];
     $mensagemHTML      = $_POST['mensagem'];
 
     if(!mail($emaildestinatario, $assunto, $mensagemHTML, $headers, "-r".$emailsender)){
         $headers .= "Return-Path: " . $emailsender;
         mail($emaildestinatario, $assunto, $mensagemHTML, $headers);
+        echo"<h3>E-mail enviado com sucesso!</h3>";
+    }else{
+        echo"<h3>Falha ao enviar o e-mail</h3>";
     }
 
 ?>
